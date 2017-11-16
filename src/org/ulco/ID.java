0ec1e78fe9f840f6e059c283ac8151011addfa03
AVASSE <avasse62410@gmail.com>
2017-11-16 14:57:53 +0100

    Feat: task 4 OK.

--- src/org/ulco/ID.java
+++ src/org/ulco/ID.java
@@ -1,5 +1,19 @@
 package org.ulco;
 
 public class ID {
-    static public int ID = 0;
+    private int ID = 0;
+
+    private ID() { }
+
+    public int getID() {
+        return ID++;
+    }
+
+    /** Instance unique pré-initialisée */
+    private static ID INSTANCE = new ID();
+
+    /** Point d'accès pour l'instance unique du singleton */
+    public static ID getInstance() {
+        return INSTANCE;
+    }
 }
