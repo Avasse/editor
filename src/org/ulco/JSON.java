4db781fa6e461daec59f1ac1fa5b0f1ad15060cb
AVASSE <avasse62410@gmail.com>
2017-11-16 13:57:48 +0100

    Feat: Second task OK.

--- src/org/ulco/JSON.java
+++ src/org/ulco/JSON.java
@@ -1,17 +1,29 @@
 package org.ulco;
 
+import java.lang.reflect.InvocationTargetException;
+
+import static java.lang.Class.forName;
+
 public class JSON {
     static public GraphicsObject parse(String json) {
         GraphicsObject o = null;
         String str = json.replaceAll("\\s+", "");
         String type = str.substring(str.indexOf("type") + 5, str.indexOf(","));
+        type = type.substring(0, 1).toUpperCase() + type.substring(1);
 
-        if (type.compareTo("square") == 0) {
-            o = new Square(str);
-        } else if (type.compareTo("rectangle") == 0) {
-            o = new Rectangle(str);
-        } else if (type.compareTo("circle") == 0) {
-            o = new Circle(str);
+        try {
+            Class cls = forName("org.ulco." + type);
+            o = (GraphicsObject) cls.getDeclaredConstructor(String.class).newInstance(str);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
         }
         return o;
     }
